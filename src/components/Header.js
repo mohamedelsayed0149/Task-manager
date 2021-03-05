25ecb2c15baa6be751b6660e9c749e19cf3e796e
mohamedelsayed0149 <mohamed11490@gmail.com>
2021-03-05 03:36:59 +0200

    initial project

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1 style={{
+        color : '#4ca6fa',
+        fontWeight : '900'
+      }}>Task Manager</h1>
+    </div>
+  )
+}
+
+export default Header
